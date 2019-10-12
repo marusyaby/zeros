db51b7660b339c9f6c6f8b6fa7e038a0a3a657ab
marusyaby <mariakhrol@gmail.com>
2019-10-12 17:43:26 +0300

    feat: add zeros function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+
+    var zeros = 0;
+    var factorials = expression.split('*');
+
+    factorials.forEach( function (factorial) {
+        if (factorial.includes('!!')) {
+            zeros = 0;
+        } else {
+            var currentNumber = parseInt(factorial);
+            while (currentNumber > 0) {
+                currentNumber = Math.floor(currentNumber / 5);
+                zeros += currentNumber;
+            }
+            console.log(factorial);
+        }
+    });
+
+    return zeros;
+};
