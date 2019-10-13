7398e390f292232eca3b92a9b5d50df6fba06c41
marusyaby <mariakhrol@gmail.com>
2019-10-13 18:47:00 +0300

    feat: add zero function for expressions

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 module.exports = function zeros(expression) {
-
-    var zeros = 0;
     var factorials = expression.split('*');
+    var fives = 0;
+    var twos = 0;
 
-    factorials.forEach( function (factorial) {
-        if (factorial.includes('!!') && parseInt(factorial)%2 !== 0) {
-            zeros += 0;
-        } else {
-            var currentNumber = parseInt(factorial);
-            while (currentNumber > 0) {
-                currentNumber = Math.floor(currentNumber / 5);
-                zeros += currentNumber;
+    factorials.forEach(function (factorial) {
+        var nextDiff = factorial.includes('!!') ? 2 : 1;
+        var number = parseInt(factorial);
+
+        for (var i = 1; i < number; number -= nextDiff) {
+            var currentNumber = number;
+
+            while (currentNumber > 1) {
+                if (currentNumber % 5 === 0) fives += 1;
+
+                currentNumber /= 5;
+            }
+
+            currentNumber = number;
+
+            while (currentNumber > 1) {
+                if (currentNumber % 2 === 0) twos += 1;
+
+                currentNumber /= 2;
             }
         }
     });
-
-    return zeros;
+    return (fives < twos) ? fives : twos;
 };
 
